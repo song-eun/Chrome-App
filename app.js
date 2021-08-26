fb367efddc4d2f69dfd85fef41657524073f4aa0
song-eun <dmswo546@naver.com>
2021-08-26 21:07:30 +0900

    update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const title = document.querySelector("h2");
 
-function handleTitleClick(){
-    console.log(h1.style.color);
-    h1.classList.toggle("clicked");
-    console.log("just clicked!");
-    console.log(h1.style.color);
-}
+const superEventHandler = {
+  HandleMouseEnter: function () {
+    title.style.color = colors[0];
+    title.innerText = "mouse is here!";
+  },
+  HandleMouseOut: function () {
+    title.style.color = colors[1];
+    title.innerText = "mouse is gone!";
+  },
+  HandleResize: function () {
+    title.style.color = colors[2];
+    title.innerText = "You just resized!";
+  },
+  HandleContextMenu: function () {
+    title.style.color = colors[3];
+    title.innerText = "That was a right click!";
+  }
+};
 
-h1.addEventListener("click",handleTitleClick);
+title.addEventListener("mouseenter", superEventHandler.HandleMouseEnter);
+title.addEventListener("mouseout", superEventHandler.HandleMouseOut);
+window.addEventListener("resize", superEventHandler.HandleResize);
+title.addEventListener("contextmenu", superEventHandler.HandleContextMenu);
