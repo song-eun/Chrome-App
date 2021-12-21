a5116b74428ee34871deba4795e9bd812523700a
song-eun <dmswo546@naver.com>
2021-12-21 18:18:42 +0900

    날씨아이콘+css

---
+++ js/weather_test.js
@@ -0,0 +1,37 @@
+
+
+const API_KEY = "4a27e63d2b9aa488914c30c2fb65ebe0";
+let weatherIcon = {
+    '01' : 'fas fa-sun',
+    '02' : 'fas fa-cloud-sun',
+    '03' : 'fas fa-cloud',
+    '04' : 'fas fa-cloud-meatball',
+    '09' : 'fas fa-cloud-sun-rain',
+    '10' : 'fas fa-cloud-showers-heavy',
+    '11' : 'fas fa-poo-storm',
+    '13' : 'far fa-snowflake',
+    '50' : 'fas fa-smog'
+};
+function onGeoOk(position) {
+
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            var $Icon = (data.weather[0].icon).substr(0,2);
+            let icon = document.querySelector(".CurrIcon");
+            const city = document.querySelector("#weather span:last-child");
+            const weather = document.querySelector("#weather span:nth-child(2)");
+            icon.innerHTML = '<i class="' + weatherIcon[$Icon] +'"></i>';
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
